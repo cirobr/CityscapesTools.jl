d21843d8b769d1a66162fb7a96d92cdebda6fa17
cirobr <ciro.rosa@alumni.usp.br>
2026-09-13 14:59:23 -0300

    cleanup tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,16 @@ using CityscapesTools; cst=CityscapesTools
 using Test
 
 @testset "CityscapesTools.jl" begin
-    include("conversions_tests.jl")
+    @test cst.classnumber2classname(0) == "road"      # 0 is NOT background
+    @test cst.classname2classnumber("road") == 0      # 0 is NOT background
+    @test cst.classnumber2classname(255) == "void"
+    @test cst.classname2classnumber("void") == 255
+    @test cst.classnumber2classname(15) == "bus"
+    @test cst.classname2classnumber("bus") == 15
+    @test cst.classnumber2classname(6) == "traffic light"
+    @test cst.classname2classnumber("traffic light") == 6
+
+    @test cst.mapping_20[0] == 255
+    @test cst.mapping_20[7] == 0
+    @test cst.mapping_20[-1] == 0
 end
