b2858af1fadd95548ad8ff217ba9efaf9b1ae66b
cirobr <ciro.rosa@alumni.usp.br>
2026-09-13 10:50:40 -0300

    test added

--- test/conversions_tests.jl
+++ test/conversions_tests.jl
@@ -1,5 +1,7 @@
+@test classnumber2classname(6) == "traffic light"
 @test classnumber2classname(15) == "bus"
 @test classnumber2classname(30) == "void"  # 30 is not a valid class
 
+@test classname2classnumber("traffic light") == 6
 @test classname2classnumber("bus") == 15
 @test classname2classnumber("unknown") == 255  # "unknown" is not a valid class
